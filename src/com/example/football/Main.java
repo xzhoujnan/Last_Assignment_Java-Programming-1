29e70572d1a4e3a0b071dc0d5caac18d190ea508
Đinh Hoài Nam <namhoanq0108@gmail.com>
2024-08-31 18:14:40 +0700

    second commit

--- src/com/example/football/Main.java
+++ src/com/example/football/Main.java
@@ -8,15 +8,3 @@ public class Main {
         TeamDisplay.display(team);
     }
 }
-package com.example.football;
-
-public class Main {
-    public static void main(String[] args) {
-        // Create a FootballPlayer object
-        FootballPlayer cristiano = new FootballPlayer("Cristiano Ronaldo", 7, "Center Forward", 80.5, 185, 39);
-
-        // Call the shoot and pass methods
-        cristiano.shoot(); // Output: Cristiano Ronaldo is shooting a ball.
-        cristiano.pass();  // Output: Cristiano Ronaldo is passing to Messi.
-    }
-}
